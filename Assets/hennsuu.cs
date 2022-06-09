c493af88c80a7dee8345d85c8b293a1c16c6dd9f
Toya <66713857+NorauDn@users.noreply.github.com>
2022-06-09 15:00:15 +0900

    Initial commit

---
+++ Assets/hennsuu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hennsuu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a= 10;
+        float fw = 3.14f;
+        string str = "‚±‚ñ‚É‚¿‚Í";
+        bool bl = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(a);
+        Debug.Log(fw);
+        Debug.Log(str);
+        Debug.Log(bl);
+
+    }
+}
